f090bc3f7bc5cdd7e3a0d16d7f0db37744da1979
yh-pengtu <69747308+yh-pengtu@users.noreply.github.com>
2023-07-28 15:11:57 +0800

    Update __init__.py

--- mmdet/core/hook/__init__.py
+++ mmdet/core/hook/__init__.py
@@ -5,10 +5,10 @@ from .set_epoch_info_hook import SetEpochInfoHook
 from .sync_norm_hook import SyncNormHook
 from .sync_random_size_hook import SyncRandomSizeHook
 from .yolox_lrupdater_hook import YOLOXLrUpdaterHook
-from .yolox_mode_switch_hook import YOLOXModeSwitchHook
+from .yolox_mode_switch_hook import YOLOXModeSwitchHook, FemtoDetModeSwitchHook
 
 __all__ = [
     'SyncRandomSizeHook', 'YOLOXModeSwitchHook', 'SyncNormHook',
     'ExpMomentumEMAHook', 'LinearMomentumEMAHook', 'YOLOXLrUpdaterHook',
-    'CheckInvalidLossHook', 'SetEpochInfoHook'
+    'CheckInvalidLossHook', 'SetEpochInfoHook', 'FemtoDetModeSwitchHook'
 ]
